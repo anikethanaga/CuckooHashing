37960438b15d4264ac36dc6ebf8b123580858913
Aniketh Anagawadi <anikethanaga@gmail.com>
2018-11-04 01:07:02 +0530

    Commit 2

--- CuckooHash.py
+++ CuckooHash.py
@@ -1,23 +1,23 @@
 import math
 class CuckooHash:
 	
-	def __init__(self):
-		self.len=100#length of tables
+	def __init__(self,size=100):
+		self.len=size#length of tables
 		self.hashtable=[[None for _ in range(self.len)] for _ in range(3)]
-		self.stash=list()#stash to store elements that create infinite loops
-		self.population=0
+		self.stash=CuckooHash(self.len//10)#stash to store elements that create infinite loops
+		self.population=0#to store number of elements in all tables combined
 
 	def stringHashFunction(self,str):
 		p1=37#a prime number
-		sum1,sum2,sum3=37,5381,0
+		sum1,sum2,sum3=37,5381,0#assigning initial values to hashvalues
 		for i in range(len(str)-1,-1,-1):
 			sum1 = (sum1+ord(str[i]))*p1#calculated using Horner's rule
 			sum2 = ((sum2 << 5) + sum2) + ord(str[i]) #djb2 algorithm
 			sum3 = ord(str[i]) + (sum3 << 6) + (sum3 << 16) - sum3#sdbm algorithm
-		sum1=sum1%self.len
+		sum1=sum1%self.len#using compression maps
 		sum2=sum2%self.len
 		sum3=sum3%self.len
-		return (sum1,sum2,sum3)
+		return (sum1,sum2,sum3)#return a tuple consisting of hashvalues
 
 	def integerHashFunction(self,key): 
 		#MAD function
@@ -40,27 +40,45 @@ class CuckooHash:
 	def insert(self,key,count=0,i=0):
 		if(isinstance(key,int)):
 			hashvalues=self.integerHashFunction(key)
+			'''the return value of integer 
+			hash function is assigned to hashvalues variable'''
 		else:
 			hashvalues=self.stringHashFunction(key)
+			'''the return value of string
+			hash function is assigned to hashvalues variable'''
 		if(count>math.floor(math.log(self.len))):
-			self.insertIntoStash(key)
+			self.stash.insert(key)				
 			return
+			'''Here we are checking if number of function calls exceeds the
+			log of table size. If it does,it indicates an infinite loop and we push into the stash'''
 		if(self.hashtable[i][hashvalues[i]]==None):
 			self.hashtable[i][hashvalues[i]]=key
+			'''if the slot to which key is hashed is empty,then we
+			simply insert the key''' 
 		else:
 			temp=self.hashtable[i][hashvalues[i]]
 			self.hashtable[i][hashvalues[i]]=key
 			self.insert(temp,count+1,(i+1)%3)
+			'''if the slot to which the key is hashed is filled,then we pop 
+			the previous element ,insert the new element and rehash the previous key
+			to alternate location in the next table''' 
 		self.population += 1
-		if self.population
+		if self.population>=(self.len*0.9*3):
+			self.rehash()
 
 	def insertIntoStash(self,key):
+		self.stash.insert()
+
 
 	def delete(self,key):
 		if isinstance(key,int):
 			index = self.integerHashFunction(key)
+			'''the return value of integer 
+			hash function is assigned to hashvalues variable'''
 		else:
 			index = self.stringHashFunction(key)
+			'''the return value of string
+			hash function is assigned to hashvalues variable'''
 		if hashtable[0][index[0]]==key:
 			hashtable[0][index[0]]=None
 		elif hashtable[1][index[1]]==key:
